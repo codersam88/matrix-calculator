f09407e5ea73eccf521330840eb7dc0eeeffeac1
sampath <sampath.surineni@gmail.com>
2014-02-20 21:54:24 +0530

    layout fixed

--- src/matrixCalc/EnterMat.java
+++ src/matrixCalc/EnterMat.java
@@ -1,12 +1,16 @@
-import android.R;
+package matrixCalc;
+
+import sam.matrixcalc.R;
 import android.app.Activity;
 import android.os.Bundle;
+
 
 
 public class EnterMat extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainentrance);
 		
 	}
 
