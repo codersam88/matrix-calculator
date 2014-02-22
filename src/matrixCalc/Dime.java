7c722e9a73f26fd86fc468bb13d21d179e39b816
sampath <sampath.surineni@gmail.com>
2014-02-22 21:17:20 +0530

    one more class added

---
+++ src/matrixCalc/Dime.java
@@ -0,0 +1,14 @@
+package matrixCalc;
+
+import sam.matrixcalc.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dime extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.matrixdim);
+	}
+
+}
--- src/matrixCalc/EnterMat.java
+++ src/matrixCalc/EnterMat.java
@@ -2,19 +2,30 @@ package matrixCalc;
 
 import sam.matrixcalc.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
 
 public class EnterMat extends Activity{
 	Button enter;
+	Dime dim = new Dime();
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		final Intent i = new Intent(this,Dime.class);
 		setContentView(R.layout.mainentrance);
 		enter = (Button)findViewById(R.id.enter_matrix);
-		enter.
+		enter.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(i);
+				
+			}
+		});
 		
 	}
 
